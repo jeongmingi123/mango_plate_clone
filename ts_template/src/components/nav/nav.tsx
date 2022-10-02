cd8380ab6bd5550df9c5de9519b8e8871b393042
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2022-10-02 13:30:02 +0900

    css running

--- ts_template/src/components/nav/nav.tsx
+++ ts_template/src/components/nav/nav.tsx
@@ -2,20 +2,24 @@ import { Link } from "react-router-dom";
 import { useMatch } from "react-router-dom";
 import styled from "styled-components";
 import tw from "tailwind-styled-components";
+import NavInnerSearch from "../nav_inner_search/nav_inner_search";
 
 export interface INavbarProps {}
 
 const NavContainer = tw.div<{ isHome: boolean }>`
-  flex
-  justify-between
   ${(props) => (props.isHome ? "nav-height" : "h-48")}
   w-full
-  bg-red-500
+  bg-orange-400
   text-white
-  text-lg
+`;
+
+const Navbar = tw.div`
+  flex
+  justify-between
   p-5
   pl-8
   pr-8
+  text-lg
 `;
 
 const Title = tw.h1`
@@ -31,7 +35,46 @@ const Icon = tw.i<{ icon: string }>`
   ${(props) => props.icon}
 `;
 
-const MenuItem = tw.h1``;
+const MenuItem = tw.h1`
+
+`;
+
+// Text
+
+const TextBox = tw.div`
+  h-80
+  flex
+  flex-col
+  items-center
+  justify-center
+`;
+
+const Text = tw.h1`
+  text-4xl
+  mt-1
+`;
+
+const InnerSearch = tw.div`
+  w-2/5
+  h-16
+  bg-white
+  rounded-full	
+  mt-14
+`;
+
+const SearchInput = tw.input`
+  w-3/4
+  h-full
+  rounded-full	
+`;
+
+const SearchBtn = tw.button`
+  w-1/4
+  h-full
+  rounded-full
+  bg-emerald-400
+  text-base
+`;
 
 export function Nav() {
   const homeMatch = useMatch("/");
@@ -39,21 +82,31 @@ export function Nav() {
   return (
     <>
       <NavContainer isHome={homeMatch !== null}>
-        <Link to="/" style={{ display: "inline-block", height: "40px" }}>
-          Sweet PLATE
-        </Link>
-        <Menu>
+        <Navbar>
           <Link to="/" style={{ display: "inline-block", height: "40px" }}>
-            EAT딜
+            Sweet PLATE
           </Link>
-          <Link to="/" style={{ display: "inline-block", height: "40px" }}>
-            맛집 리스트
-          </Link>
-          <Link to="/" style={{ display: "inline-block", height: "40px" }}>
-            Sweet 스토리
-          </Link>
-          <Icon icon="fa-solid fa-user" />
-        </Menu>
+          <Menu>
+            <Link to="/" style={{ display: "inline-block", height: "40px" }}>
+              EAT딜
+            </Link>
+            <Link to="/" style={{ display: "inline-block", height: "40px" }}>
+              맛집 리스트
+            </Link>
+            <Link to="/" style={{ display: "inline-block", height: "40px" }}>
+              Sweet 스토리
+            </Link>
+            <Icon icon="fa-solid fa-user" />
+          </Menu>
+        </Navbar>
+        <TextBox>
+          <Text>솔직한 리뷰, 믿을 수 있는 평점!</Text>
+          <Text>망고 플레이트</Text>
+          <InnerSearch>
+            <SearchInput></SearchInput>
+            <SearchBtn>검색</SearchBtn>
+          </InnerSearch>
+        </TextBox>
       </NavContainer>
     </>
   );
--- ts_template/src/components/nav_inner_search/nav_inner_search.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NavInnerSearch = () => {
-  return <></>;
-};
-
-export default NavInnerSearch;
