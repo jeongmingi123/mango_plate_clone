30792006dce7614977fa49c54172fda5ae37dc3a
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2022-10-04 20:05:14 +0900

    Update nav.tsx

--- ts_template/src/components/nav/nav.tsx
+++ ts_template/src/components/nav/nav.tsx
@@ -17,6 +17,7 @@ const Navbar = tw.div<{ isOverTextBox: boolean }>`
   ${(props) => (props.isOverTextBox ? "bg-white" : "bg-orange-400")}
   ${(props) => (props.isOverTextBox ? "shadow-2xl" : "")}
   ${(props) => (props.isOverTextBox ? "shadow-orange-500/40" : "")}
+  ${(props) => (props.isOverTextBox ? "border-b-2" : "")}
   flex
   w-full
   justify-between
